8d4025883f85f1597842873d0b36748631412fb3
Nick Partridge <nkpart@gmail.com>
2010-01-29 21:31:53 +1000

    more gae cleanups

--- src/main/scala/gae/package.scala
+++ src/main/scala/gae/package.scala
@@ -1,32 +1,7 @@
-import com.google.appengine.api.datastore._
-import com.google.appengine.api.users.{UserService, UserServiceFactory, User}
 import gae._
 
 import scalaz._
 import Scalaz._
-
-package gae {
-  trait Kind[T] {
-    def kind: String
-  }
-
-  // Entities need to be created 
-  trait EntityBase[T] extends Kind[T] {
-    def keyName(t: T): Option[String]
-
-    def createBase(t: T, parentKey: Option[Key]): Entity = {
-      (keyName(t), parentKey) match {
-        case (Some(n), Some(pk)) => new Entity(kind, n, pk)
-        case (Some(n), None) => new Entity(kind, n)
-        case (None, Some(pk)) => new Entity(kind, pk)
-        case (None, None) => new Entity(kind)
-      }
-    }
-  }
-
-  trait EntityWriteable[T] { def write(t: T, e: Entity) }
-  trait EntityCreatable[T] { def createFrom(e: Entity): Option[T] }
-}
 
 package object gae extends 
   IdentityImplicits with
@@ -35,6 +10,7 @@ package object gae extends
   UserServiceImplicits 
   {
 
+  import com.google.appengine.api.datastore._
   def createQuery[T](implicit s: Kind[T]) = new Query(s.kind)
   def createKey[T](name: String)(implicit s : Kind[T]) = KeyFactory.createKey(s.kind, name)
   def createKey[T](id: Long)(implicit s : Kind[T]) = KeyFactory.createKey(s.kind, id)
