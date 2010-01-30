ec8658769ac6a667e00599672e51ae4ec5b6e2d2
Nick Partridge <nkpart@gmail.com>
2010-01-30 10:45:44 +1000

    fiddling with dsl for queries

--- src/main/scala/wd/Brewery.scala
+++ src/main/scala/wd/Brewery.scala
@@ -2,6 +2,8 @@ package wd
 
 import com.google.appengine.api.datastore._
 import com.google.appengine.api.datastore.Query.SortDirection
+import com.google.appengine.api.datastore.Query.SortDirection._
+import com.google.appengine.api.datastore.Query.FilterOperator._
 import gae._
 import scalaz._
 import Scalaz._
@@ -10,9 +12,22 @@ import Scalaz._
 // * full address.
 // isPub
 
-case class Country(value: String)
+case class Country(value: String) extends NewType[String]
 case class Brewery(name: String, country: Country)
 
 object Brewery {
-  val allByName = Find[Brewery].query(_.addSort("name", SortDirection.ASCENDING)).iterable _
+  
+  implicit def dsl(field: String) = new {
+    import com.google.appengine.api.datastore.Query.SortDirection._
+    import com.google.appengine.api.datastore.Query.FilterOperator._
+    
+    type QRY = Query => Query
+    
+    def asc: QRY = (_.addSort(field, ASCENDING))
+    def ?==[T](t: T): QRY = (_.addFilter(field, EQUAL, t))
+  }
+  
+  val allByName = Find[Brewery].query("name".asc).iterable _
+  
+  def allInCountry(c: Country) = Find[Brewery].query("country" ?== c.value).iterable _
 }
